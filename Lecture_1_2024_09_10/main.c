055f5af2d6f75cdf35897672e568441c85ec7629
Forkyouabhi <forkyouabhi@proton.me>
2024-09-10 14:07:03 -0400

    Learnt how indexing work on RAM level

--- Lecture_1_2024_09_10/main.c
+++ Lecture_1_2024_09_10/main.c
@@ -2,6 +2,11 @@
 
 int main(){
   int a[10];
-  printf("%lu",(unsigned long)&a[0]);     
+  printf("%lu",(unsigned long)&a[0]);   
+//   for(int i; i<10; i++){
+//     printf("%lu ",(unsigned long)&a[i]);
+//   }; 
+    // int *ptr= &a;
+    printf(" The size of a pointer on this machine is %d",sizeof(int *));
   return 0;
 }
