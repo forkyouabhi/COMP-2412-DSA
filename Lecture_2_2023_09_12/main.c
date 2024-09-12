3d5246cc8b1f79651c39e59ce19bd06c8a0fc5c2
Forkyouabhi <forkyouabhi@proton.me>
2024-09-12 13:44:04 -0400

    addresses of dynamic arrays and elements

--- Lecture_2_2023_09_12/main.c
+++ Lecture_2_2023_09_12/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(){
     // int staticArrayAbhi[]={1,2,3,4,5};
     // printf("%d",staticArrayAbhi[0]);
 
     int a[10]; //static array
-    int *arr=(int *)malloc(10*sizeof(int)); //diynamically created array
+    // int *arr=(int *)malloc(10*sizeof(int)); //diynamically created array
+    int *b=(int *)malloc(10*sizeof(int)); //diynamically created array
     a[0]=100;
     a[9]=1000;
-    arr[0]=200;
-    printf("Hello World");
+    // arr[0]=200;
+    b[0]=200;
+    printf("%X and %X and %X",&a[0],&a[0],&a,a); //findind the adress of those dynamic arrays.
+    printf("%X and %X and %X",&a[0],&b[0],&b,b); //findind the adress of those dynamic arrays.
+    // printf("%x and %X",&a[0],&a[9]); //findind the adress of those dynamic arrays.
+
+    // printf("\n%d\n",*arr);
     return 0;
 }
